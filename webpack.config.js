0f7ffa1bfae3fba9a644c72fdbdab4694a51ae97
TanninOne <seppleviathan@gmx.de>
2023-01-24 11:01:32 +0100

    build fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('modtype-harmony-patcher', __dirname, 4);
+module.exports = webpack('modtype-harmony-patcher', __dirname, 5);
